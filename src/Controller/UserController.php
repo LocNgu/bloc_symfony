4d3ce710d6e06cbef8fff54726f585047cb64daa
LocNgu <loc.nguyen_dang_duc@tu-dresden.de>
2019-12-13 16:31:37 +0100

    add user view

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,15 +1,28 @@
 <?php
+
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     public function index()
     {
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository(User::class)->findAll();
         $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+
         return $this->render(
-            'admin/admin.html.twig'
+            'admin/user/user.html.twig',
+            ['users' => $users]
         );
     }
+    public function create()
+    {
+        return $this->render(
+            'admin/user/create_user.html.twig'
+        );
+    }
+
 }
