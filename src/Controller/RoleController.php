af2fa8464a842b3274a6687a07c83438af0dd6fe
LocNgu <loc.nguyen_dang_duc@tu-dresden.de>
2019-12-16 15:43:18 +0100

    change redirect

--- src/Controller/RoleController.php
+++ src/Controller/RoleController.php
@@ -9,8 +9,6 @@ class RoleController extends AbstractController
     public function index()
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
-
-
         return $this->render(
             'admin/user/role.html.twig'
         );
